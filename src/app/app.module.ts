029a656ebdacbda0d0e27d4307de3c2cea8b852b
Alan Bessa <bessa.alan@gmail.com>
2017-03-23 23:16:28 -0300

    Formularios Basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 //Rotas
@@ -38,6 +38,7 @@ import { CartPageComponent } from './pages/cart-page/cart-page.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     Routing
   ],
