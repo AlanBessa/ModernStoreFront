7cec232585fd07f0de56f8dd5c52b816da82f5e4
Alan Bessa <bessa.alan@gmail.com>
2017-03-24 00:10:22 -0300

    Validações Customizadas

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { CustomValidator } from '../../validators/custom.validator';
+import { Ui } from '../../utils/ui';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
+  providers: [Ui]
 })
 export class LoginPageComponent implements OnInit {
   public form: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private ui: Ui) { 
     this.form = this.fb.group({
       email: ['', Validators.compose([
         Validators.minLength(6),
         Validators.maxLength(160),
-        Validators.required
+        Validators.required,
+        CustomValidator.EmailValidator
       ])],
       password: ['', Validators.compose([
         Validators.minLength(6),
@@ -26,4 +30,22 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  checkEmail() {
+    this.ui.lock('emailControl');
+    this.form.controls['email'].disable;    
+
+    setTimeout(() => {
+      this.ui.unlock('emailControl');
+      this.form.controls['email'].enable;
+      console.log(this.form.controls['email'].value);
+    }, 3000);    
+  }
+
+  showModal() {
+    this.ui.setActive('modalControl');
+  }
+
+  hideModal() {
+    this.ui.setInactive('modalControl');
+  }
 }
